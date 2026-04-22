b2728bba201007afa76ba3a9763d8e8d5ead6d78
FromZeroToSec <mehdibenabdel952@gmail.com>
2026-04-22 05:44:55 +0200

    init: project structure

---
+++ 03-port-quiz-cli/main.py
@@ -0,0 +1,31 @@
+from random import choice
+
+
+
+PORTS = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    445: "SMB",
+    1433: "MSSQL",
+    1521: "Oracle",
+    3306: "MySQL",
+    3389: "RDP",
+    5432: "PostgreSQL",
+    6379: "Redis",
+    8080: "HTTP-alt",
+    8443: "HTTPS-alt",
+    27017: "MongoDB",
+    2375: "Docker",
+    2376: "Docker-TLS",
+    5000: "Flask",
+    9200: "Elasticsearch",
+    5601: "Kibana",
+    22222: "SSH-alt",
+}
