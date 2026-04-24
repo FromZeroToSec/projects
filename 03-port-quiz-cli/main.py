84d00387a8351d092f61e53850ce0d873f51a8c5
Mehdi <mehdibenabdel952@gmail.com>
2026-04-24 18:12:17 +0200

    feat: add win and lose conditions with replay option

--- 03-port-quiz-cli/main.py
+++ 03-port-quiz-cli/main.py
@@ -29,3 +29,37 @@ PORTS = {
     5601: "Kibana",
     22222: "SSH-alt",
 }
+
+attempts = 0
+max_attempts = 5
+
+def run_quiz():
+    port = choice(list(PORTS.keys()))
+    service = PORTS[port]
+        
+    print(f"What service runs on port {port}?")
+    answer = input("Your answer: ")
+    return service, answer , port
+
+while True:
+    attempts = 0
+    while attempts < max_attempts:
+        service, answer , port = run_quiz()
+        if service == answer:
+            print("You Win ! ")
+            again = input(f"Play again? (y/n): ").lower()
+            if again == "n":
+                break
+        else:
+            attempts +=1
+            remaining = max_attempts - attempts
+            print(f"You have {remaining} attempt(s) left")
+
+            if remaining == 0:
+                print(f"You lose! The correct answer was {port}")
+                again = input("Play again? (y/n): ").lower()
+                if again == "n":
+                    break
+    if again == "n":
+        break
+    
