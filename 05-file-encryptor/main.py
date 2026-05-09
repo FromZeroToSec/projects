5c60abbb0f2b66e73e663b75479d8c6d6a67aac9
Mehdi <mehdibenabdel952@gmail.com>
2026-05-09 15:15:35 +0200

    feat: add file encryption function

--- 05-file-encryptor/main.py
+++ 05-file-encryptor/main.py
@@ -1,9 +1,19 @@
-from cryptography.fernet import Fernet #Module for encryption #secret.key gitignore
+from cryptography.fernet import Fernet #Module for encryption
 
 
-def generate_key():#Generate a key
-    key = Fernet.generate_key()
+def generate_key():
+    key = Fernet.generate_key()#Generate the key
     with open("secret.key","wb") as file:#Write the key, "wb" for binary
         file.write(key)#Write the key to the file
 
-generate_key()#Call the function        
+def encrypt_file(file_name):    
+    with open("secret.key","rb") as key_file:  #Open the file, "rb" for binary
+        key = key_file.read() #read the file
+    with open(file_name,"rb") as file:#Open the file, "rb" for binary:
+        fernet = Fernet(key)#Create the Fernet object
+        encrypted = fernet.encrypt(file.read())#Encrypt the file
+    with open (file_name,"wb") as file:#Open the file, "wb" for binary
+        file.write(encrypted)#Write the encrypted file
+
+generate_key()#Call the function  
+encrypt_file("test.txt")#Call the function
