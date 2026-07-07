802b305dc96c49204071f1dc52434ebd15397179
Mehdi <mehdibenabdel952@gmail.com>
2026-07-07 15:55:01 +0200

    feat: add JSON persistence for tasks (load/save)

--- 07-todo-cli/main.py
+++ 07-todo-cli/main.py
@@ -1,14 +1,19 @@
-import sys
 import json
 
 try:
-    file = open("tasks.json", "r")#ouverture du fichier
-    tasks = json.load(file)#chargement du fichier
-    file.close()
+    with open("tasks.json", "r", encoding="utf-8") as file:
+        tasks = json.load(file)
     print("The file has been loaded.")
-except FileNotFoundError:#fichier n'existe pas
-    tasks = []#creation du fichier
+except FileNotFoundError:
+    tasks = []
     print("The file does not exist. It will be created.")
-except json.JSONDecodeError:  # fichier existe mais vide/corrompu
-    tasks = []#creation du fichier
+except json.JSONDecodeError:
+    tasks = []
     print("The file is empty or corrupted. It will be created.")
+
+def save_tasks(tasks):
+    with open("tasks.json", "w", encoding="utf-8") as file:
+        json.dump(tasks, file)
+    print("The file has been saved.")
+
+save_tasks(tasks)
