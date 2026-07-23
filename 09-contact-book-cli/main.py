b519034643f1c9c75d8895b54a0dc7a01035ed9f
Mehdi <mehdibenabdel952@gmail.com>
2026-07-24 01:57:54 +0200

    feat: add contact creation and listing with menu loop

---
+++ 09-contact-book-cli/main.py
@@ -0,0 +1,37 @@
+def get_contacts():
+    name = input("Name: ")
+    phone = input("Phone: ")
+    email = input("Email: ")
+    return name , phone , email
+
+def add_contact(contacts, name, phone, email):
+    contact = {
+        "name": name,
+        "phone": phone,
+        "email": email
+    }
+    contacts.append(contact)
+
+def main():
+    contacts = []
+    print("Welcome to the contact book")
+    while True:
+        print("1. Add contact")
+        print("2. View contacts")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            name, phone, email = get_contacts()
+            add_contact(contacts, name, phone, email)
+        elif choice == "2":
+            for contact in contacts:
+                print(f"Name: {contact['name']}")
+                print(f"Phone: {contact['phone']}")
+                print(f"Email: {contact['email']}")
+                print()
+        elif choice == "3":
+            break
+
+
+if __name__ == "__main__":
+    main()
