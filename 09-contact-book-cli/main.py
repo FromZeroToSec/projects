5ac9eddd1a6821111f7d017a138e4393cb70bf33
Mehdi <mehdibenabdel952@gmail.com>
2026-07-24 02:02:49 +0200

    feat: add contact creation, listing and input validation

--- 09-contact-book-cli/main.py
+++ 09-contact-book-cli/main.py
@@ -20,17 +20,24 @@ def main():
         print("2. View contacts")
         print("3. Exit")
         choice = input("Enter your choice: ")
-        if choice == "1":
+        try:
+            choice = int(choice)
+        except ValueError:
+            print("Invalid choice")
+            continue
+        if choice == 1:
             name, phone, email = get_contacts()
             add_contact(contacts, name, phone, email)
-        elif choice == "2":
+        elif choice == 2:
             for contact in contacts:
                 print(f"Name: {contact['name']}")
                 print(f"Phone: {contact['phone']}")
                 print(f"Email: {contact['email']}")
                 print()
-        elif choice == "3":
+        elif choice == 3:
             break
+        else:
+            print("Invalid choice")
 
 
 if __name__ == "__main__":
