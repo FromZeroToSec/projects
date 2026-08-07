7f870c81b4757156bf07998f9dd852d6ca92693d
Mehdi <mehdibenabdel952@gmail.com>
2026-08-07 11:23:35 +0200

    feat: validate log level before writing entry

--- 10-log-entry-writer/main.py
+++ 10-log-entry-writer/main.py
@@ -1,11 +1,19 @@
 from datetime  import datetime
 
 
-def write_log (message, level):
-    current_time = datetime.now()
-    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    with open('app.log', 'a') as f:
-        f.write(f'[{formatted_time}] [{level}] {message}\n')
+def write_log(message, level):
+    allowed_levels = ["INFO", "WARNING", "ERROR"]
+    if level in allowed_levels:
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        with open('app.log', 'a') as f:
+            f.write(f'[{formatted_time}] [{level}] {message}\n')
+    else:
+        print('Invalid log level')
+
+
+
 
 if __name__ == '__main__':
     write_log('Hello world', 'INFO')
+
