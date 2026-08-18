f1a1100ab130835bae62e11b8ede32839f0998f1
Mehdi <mehdibenabdel952@gmail.com>
2026-08-18 10:39:32 +0200

    feat: add disk usage info

--- 11-system-info-collector/main.py
+++ 11-system-info-collector/main.py
@@ -15,7 +15,10 @@ def main():
     print(f"Logical cores: {psutil.cpu_count(logical=True)}")
     print(f"Memory usage: {psutil.virtual_memory().percent}%")
     print(f"Total RAM (GB): {round(psutil.virtual_memory().total / (1024 ** 3), 2)}")
-
+    print(f"Available RAM (GB): {round(psutil.virtual_memory().available / (1024 ** 3), 2)}")
+    print(f"Disk usage: {psutil.disk_usage('/').percent}%")
+    print(f"Total disk space (GB): {round(psutil.disk_usage('/').total / (1024 ** 3), 2)}")
+    print(f"Available disk space (GB): {round(psutil.disk_usage('/').free / (1024 ** 3), 2)}")
 
 
 if __name__ == "__main__":
