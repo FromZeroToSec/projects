c886b700f8715fec60e97ad962e1e823a0159ed2
Mehdi <mehdibenabdel952@gmail.com>
2026-08-24 10:28:09 +0200

    feat: extract iterative fibonacci into its own function

--- 16-fibonacci-generator/main.py
+++ 16-fibonacci-generator/main.py
@@ -1,28 +1,33 @@
 import time
 
-def main():
-    start = time.time()
-    a = 0
-    b = 1
-    for i in range(10):
-        print(a)
-        old_a = a
-        a = a + b
-        b = old_a
-        print(fibonacci_recursive(i))
-    print(f"Iterative time: {time.time() - start}")
-    start = time.time()
-    print(fibonacci_recursive(10))
-    print(f"Recursive time: {time.time() - start}")
-
 
 def fibonacci_recursive(n):
     if n == 0:
         return 0
     elif n == 1:
-        return 1 
+        return 1
     else:
         return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+
+def fibonacci_iterative(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        old_a = a
+        a = a + b
+        b = old_a 
+    return a
+
+
+
+def main():
+    start = time.time()
+    print("Résultat itératif :", fibonacci_iterative(30))
+    print("Temps itératif :", time.time() - start)
+    start = time.time()
+    print("Résultat récursif :", fibonacci_recursive(30))
+    print("Temps récursif :", time.time() - start)
 
 
 if __name__ == "__main__":
