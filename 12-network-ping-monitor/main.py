64f1d16bca153dc1570c993673dcd933ef8beb58
Mehdi <mehdibenabdel952@gmail.com>
2026-08-28 09:27:56 +0200

    feat: add user input for address to ping

--- 12-network-ping-monitor/main.py
+++ 12-network-ping-monitor/main.py
@@ -1,4 +1,17 @@
 import subprocess
 
-result = subprocess.run(["ping", "-c", "1", "8.8.8.8"], capture_output=True, text=True)
-print(result.stdout)
+
+def check_ping(address):
+    """check if an address is reachable"""
+    result = subprocess.run(["ping", "-c", "1", address], capture_output=True, text=True)
+    return result.returncode == 0 # if the return code is 0, the address is reachable
+
+def main():
+    address = input("Which address do you want to check? ")
+    if check_ping(address):
+        print("Available")
+    else:
+        print("Not available")
+
+if __name__ == "__main__":
+    main()
