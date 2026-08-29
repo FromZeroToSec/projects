71bfef1bdca7583d487c008a9d88d7f11d3196b4
Mehdi <mehdibenabdel952@gmail.com>
2026-08-29 10:59:58 +0200

    feat: support checking multiple addresses in a loop

--- 12-network-ping-monitor/main.py
+++ 12-network-ping-monitor/main.py
@@ -6,12 +6,25 @@ def check_ping(address):
     result = subprocess.run(["ping", "-c", "1", address], capture_output=True, text=True)
     return result.returncode == 0 # if the return code is 0, the address is reachable
 
+
 def main():
-    address = input("Which address do you want to check? ")
-    if check_ping(address):
-        print("Available")
-    else:
-        print("Not available")
+    address_list = []
+    while True:
+        address = input("Which address do you want to check? ")
+        address_list.append(address)
+        continuer = input("Add another address? (Y/N): ")
+        if continuer == "Y".lower():
+            continue
+        elif continuer == "N".lower():
+            break
+        else:
+            print("Invalid input")
+    for address in address_list:
+        if check_ping(address):
+            print(f"{address} is Available")
+        else:
+            print(f"{address} is Not available")
+
 
 if __name__ == "__main__":
     main()
