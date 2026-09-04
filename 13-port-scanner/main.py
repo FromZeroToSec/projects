d7ca9452f398321206db200a0dac592b79a0cfa4
Mehdi <mehdibenabdel952@gmail.com>
2026-09-04 12:18:28 +0200

    feat: add service name lookup for open ports

--- 13-port-scanner/main.py
+++ 13-port-scanner/main.py
@@ -2,12 +2,24 @@ import socket
 
 
 host = "127.0.0.1"
-for port in range(1, 1025):
+
+for port in range(1,1025):
+    #créer socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #tester connexion
     result = s.connect_ex((host, port))
+    #fermer socket
+    s.close()
+    #si port ouvert:
     if result == 0:
-        print(f"{port} open")
+        #chercher le service (try/except)
+        try:
+            service = socket.getservbyport(port, "tcp")
+        except OSError:
+            service = "Unknown"
+    # afficher port + service + "open"
+        print(f"{port} {service} open")
+    #sinon:
     else:
         print(f"{port} closed")
-    s.close()
 
