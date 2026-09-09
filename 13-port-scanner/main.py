1c1f8b45a8fbf72e6489805e39a2aca0cf9f155a
Mehdi <mehdibenabdel952@gmail.com>
2026-09-09 11:23:52 +0200

    refactor: remove redundant host validation from scan_port

--- 13-port-scanner/main.py
+++ 13-port-scanner/main.py
@@ -7,16 +7,13 @@ def scan_port(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     timeout = 1
     s.settimeout(timeout)
-    try:
-        result = s.connect_ex((host, port))
-        s.close()
-        if result == 0:
-            return True
-        else:
-            return False
-    except socket.gaierror:
-        print(f"Error: could not resolve host '{host}'")
+    result = s.connect_ex((host, port))
+    s.close()
+    if result == 0:
+        return True
+    else:
         return False
+
 
 
 def get_service(port):
@@ -29,18 +26,25 @@ def get_service(port):
 def main():
     start_time = time.time()
     host = sys.argv[1]
-    start_port = int(sys.argv[2])
-    end_port = int(sys.argv[3])
-    for port in range(start_port, end_port + 1):
-        if scan_port(host, port):
-            service = get_service(port)
-            print(f"{port} {service} open")
-    end_time = time.time()
-    duration = end_time - start_time
-    print(f"Scan completed in {duration:.2f} seconds")
+    try:
+        socket.gethostbyname(host)
+    except socket.gaierror:
+        print(f"Error: could not resolve host '{host}'")
+        sys.exit()
+    try:
+        start_port = int(sys.argv[2])
+        end_port = int(sys.argv[3])
+        for port in range(start_port, end_port + 1):
+            if scan_port(host, port):
+                service = get_service(port)
+                print(f"{port} {service} open")
+        end_time = time.time()
+        duration = end_time - start_time
+        print(f"Scan completed in {duration:.2f} seconds")
+    except ValueError:
+        print("Error: Invalid port range")
 
 
 
 if __name__ == "__main__":
     main()
-
