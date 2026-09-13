fc0363987eb9cf07719989558d940a20c02107b9
Mehdi <mehdibenabdel952@gmail.com>
2026-09-13 14:12:33 +0200

    feat: add mx record lookup

--- 14-dns-lookup-tool/main.py
+++ 14-dns-lookup-tool/main.py
@@ -1,8 +1,16 @@
 import socket
+import dns.resolver
 
 
 def resolve_domain(domain):
     return socket.gethostbyname(domain)
+
+
+def get_mx_record(domain):
+    mx_list = []
+    for record in dns.resolver.resolve(domain, "MX"):
+        mx_list.append(f"{record.preference} {record.exchange}")
+    return mx_list
 
 
 def main():
@@ -12,6 +20,13 @@ def main():
         print(f"The IP address for {domain} is: {ip_address}")
     except socket.gaierror:
         print("Enter a valid domain name.")
+    try:
+        mx_records = get_mx_record(domain)
+        for mx in mx_records:
+            print(f"MX record: {mx}")
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
+        print(f"No MX records found for {domain}.")
+
 
 
 if __name__ == "__main__":
